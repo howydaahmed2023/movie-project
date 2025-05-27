19a6fcdf13deaa4a78fe2979d81d9601f129dba1
howyda ahmed <howydaahmedabueila@gmail.com>
2025-05-27 12:53:50 +0300

    Auto-generated commit

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-SGBVBHSL.mjs";import"./chunk-SGZNQ47J.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
+import{a}from"./chunk-USYQ4FVS.mjs";import"./chunk-2ENTSG2Z.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
